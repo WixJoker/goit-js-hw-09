1cbc36f25f760d101404fb614b4fa4962ad9f850
WixJoker(andriilytvyn) <96374210+WixJoker@users.noreply.github.com>
2022-07-26 00:17:36 +0300

    1st task has been done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const buttonStartRef = document.querySelector('button[data-start]');
+const buttonStopRef = document.querySelector('button[data-stop]');
 
+let timetId = null;
+
+buttonStartRef.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  if (timerId) {
+    buttonStartRef.disabled = 'true';
+  }
+});
+
+buttonStopRef.addEventListener('click', () => {
+  clearInterval(timerId);
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
