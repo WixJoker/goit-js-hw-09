5ec24c5f1fc1f9c3711900f45b5185f97a4634b7
WixJoker(andriilytvyn) <96374210+WixJoker@users.noreply.github.com>
2022-07-30 16:59:04 +0300

    second task has been performed

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,65 +1,81 @@
 import flatpickr from 'flatpickr';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import 'flatpickr/dist/flatpickr.min.css';
-
-const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
-  },
+import 'flatpickr/dist/themes/dark.css';
+let selectedTime = null;
+const refs = {
+  inputDate: document.querySelector('#datetime-picker'),
+  startBtn: document.querySelector('button[data-start]'),
+  days: document.querySelector('span[data-days]'),
+  hours: document.querySelector('span[data-hours]'),
+  minutes: document.querySelector('span[data-minutes]'),
+  seconds: document.querySelector('span[data-seconds]'),
 };
-
-flatpickr('#datetime-picker', options);
-
-const startButton = document.querySelector('button[data-start]');
-startButton.addEventListener('click', startTimer);
-
-const chosenDate = new Date();
-const currentDate = new Date();
-const delta = chosenDate - currentDate;
-
-const daysLeft = document.querySelector('span[data-days]');
-const hoursLeft = document.querySelector('span[data-hours]');
-const minutesLeft = document.querySelector('span[data-minutes]');
-const secondsLeft = document.querySelector('span[data-seconds]');
-
-daysLeft.textContent = days;
-hoursLeft.textContent = hours;
-minutesLeft.textContent = minutes;
-secondsLeft.textContent = seconds;
-
-function startTimer() {
-  selectedDates.forEach(() => {
-    if (delta <= 0) {
-      window.alert('Please choose a date in the future');
-      startButton.disabled = 'true';
-    }
-    convertMs(ms);
-  });
-}
-
 function convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
-
   // Remaining days
-  const days = Math.floor(ms / day);
+  const days = pad(Math.floor(ms / day));
   // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
+  const hours = pad(Math.floor((ms % day) / hour));
   // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const minutes = pad(Math.floor(((ms % day) % hour) / minute));
   // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
+  const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
   return { days, hours, minutes, seconds };
 }
-console.log(seconds);
-
-function addLeadingZero(value) {
-  value.padStart(2, '0');
+function pad(value) {
+  return String(value).padStart(2, '0');
 }
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: Date.now(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] < Date.now()) {
+      Notify.failure('Please choose a date in the future');
+      selectedDates[0] = new Date();
+    } else {
+      refs.startBtn.disabled = false;
+      selectedTime = selectedDates[0];
+    }
+  },
+};
+class Timer {
+  constructor() {
+    this.timerID = null;
+    this.isActive = false;
+    refs.startBtn.disabled = true;
+  }
+  startTimer() {
+    if (this.isActive) {
+      return;
+    }
+    this.isActive = true;
+    this.timerID = setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = selectedTime - currentTime;
+      const componentsTimer = convertMs(deltaTime);
+      this.updateComponentsTimer(componentsTimer);
+      if (deltaTime <= 0) {
+        this.stopTimer();
+      }
+    }, 1000);
+  }
+  updateComponentsTimer({ days, hours, minutes, seconds }) {
+    refs.days.textContent = days;
+    refs.hours.textContent = hours;
+    refs.minutes.textContent = minutes;
+    refs.seconds.textContent = seconds;
+  }
+  stopTimer() {
+    clearInterval(this.timerID);
+  }
+}
+const timer = new Timer();
+flatpickr(refs.inputDate, options);
+refs.startBtn.addEventListener('click', () => timer.startTimer());
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,16 +1,28 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     // Fulfill
+//   } else {
+//     // Reject
+//   }
+// }
 
-createPromise(2, 1500)
-  .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
+// const firstDelay = 1000;
+// let delay = firstDelay;
+// for (let = 0; i < 3; i += 1) {
+//   new Promise(resolve => {
+//     createPromise();
+//     setTimeout(() => {
+//       resolve(i), delay;
+//     });
+//   }).then(value => console.log(value));
+//   delay = +step;
+// }
+
+// createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
