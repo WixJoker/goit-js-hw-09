8a5af6f06e902cfd5c7ad13cfe2fc703516fa406
WixJoker(andriilytvyn) <96374210+WixJoker@users.noreply.github.com>
2022-07-30 20:35:03 +0300

    3rd task has been reviewed

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,28 +1,30 @@
-// function createPromise(position, delay) {
-//   const shouldResolve = Math.random() > 0.3;
-//   if (shouldResolve) {
-//     // Fulfill
-//   } else {
-//     // Reject
-//   }
-// }
+const firstDelayRef = document.querySelector('[name="delay"]');
+const delayStepRef = document.querySelector('[name="step"]');
+const amountRef = document.querySelector('[name="amount"]');
+const createPromisesButton = document.querySelector('button');
 
-// const firstDelay = 1000;
-// let delay = firstDelay;
-// for (let = 0; i < 3; i += 1) {
-//   new Promise(resolve => {
-//     createPromise();
-//     setTimeout(() => {
-//       resolve(i), delay;
-//     });
-//   }).then(value => console.log(value));
-//   delay = +step;
-// }
+createPromisesButton.addEventListener('click', createPromise);
 
-// createPromise(2, 1500)
-//   .then(({ position, delay }) => {
-//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  if (shouldResolve) {
+    for (let i = 0; i < 3; i += 1) {
+      new Promise(resolve => {
+        createPromise();
+        setTimeout(() => {
+          resolve(i);
+        }, delay);
+      });
+      //   .then(value => console.log(value));
+      // delay += step;
+    }
+  }
+}
+
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
