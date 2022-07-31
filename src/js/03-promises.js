dde98c17a19c5ba7ee595a4995ad59e060e55ca3
WixJoker(andriilytvyn) <96374210+WixJoker@users.noreply.github.com>
2022-07-31 17:05:27 +0300

    Third task has been done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,29 +1,47 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const createPromisesForm = document.querySelector('.form');
+const form = document.querySelector('form');
 
-createPromisesForm.addEventListener('submit', createPromise);
+form.addEventListener('submit', createPromisesOnSubmit);
+
+function createPromisesOnSubmit(event) {
+  event.preventDefault();
+  const { delay, step, amount } = event.target;
+
+  const deltaTime = parseInt(delay.value);
+  const stepTime = parseInt(step.value);
+  const amountNumber = parseInt(amount.value);
+
+  setTimeout(() => {
+    countPromises(amountNumber, stepTime);
+  }, deltaTime);
+}
+
+function countPromises(count, stepTime) {
+  let time = 0;
+
+  for (let i = 1; i <= count; i += 1) {
+    time += stepTime;
+    createPromise(i, time)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+}
 
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
-    setTimeout(() => {
+
+    setInterval(() => {
       if (shouldResolve) {
-        for (let i = 0; i < 3; i += 1) {
-          i = position;
-          resolve(position => console.log(position));
-        }
+        resolve({ position, delay });
       } else {
-        reject('error!');
+        reject({ position, delay });
       }
     }, delay);
   });
 }
-
-createPromise(2, 1500)
-  .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
